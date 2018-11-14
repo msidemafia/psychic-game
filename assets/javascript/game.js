538c47ee08633e169d723224517efe1ec85d9fbe
Chesterfield <boyce.davidw@gmail.com>
2018-11-14 15:31:57 -0500

    initial commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,6 @@
+var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+var randomNumber = Math.floor(Math.random() * letters.length);
+var computerGuess = letters[randomNumber]
+console.log(randomNumber)
+console.log(computerGuess)
