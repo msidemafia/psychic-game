b853d0e7ad4432799e9b7ffd73a041c6243e9181
Chesterfield <boyce.davidw@gmail.com>
2018-11-19 22:01:29 -0500

    second commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,56 @@
 var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var currentGuesses = []
 
-var randomNumber = Math.floor(Math.random() * letters.length);
-var computerGuess = letters[randomNumber]
-console.log(randomNumber)
-console.log(computerGuess)
+var randomNumberOne = Math.floor(Math.random() * letters.length);
+var computerChoice = letters[randomNumberOne]
+console.log(randomNumberOne)
+console.log(computerChoice)
+
+var wins = 0;
+var guessesLeft = 9;
+var losses = 0;
+var userGuess = ''
+
+var updateCurrentGuesses = function (){
+    currentGuesses.push(userGuess)
+}
+
+document.onkeyup = function(event) {
+
+    var userGuess = event.key;
+// Win scenario
+    if (userGuess == computerChoice) {
+        wins++;
+        currentGuesses = [];
+        guessesLeft = 9;
+    } 
+// Wrong guess
+    else if (userGuess !== computerChoice) {
+        updateCurrentGuesses();
+        guessesLeft--;
+        $("#guessLeftValue").text("Guesses left: " + guessesLeft)
+    }
+// Loss scenario
+    else if (guessesLeft == 0) {
+        losses++;
+        currentGuesses.length = 0;
+        reset;
+    }
+
+    console.log(userGuess)
+
+    var reset = function() {
+        guessesLeft = 9
+    }
+
+    $("#guessSoFar").text(currentGuesses)
+
+    
+    
+
+
+console.log(guessesLeft);
+console.log(wins);
+console.log(losses);
+    
+  };
